d44333f8cfec8692a0d26ad99ef868d9ba5954c4
Searchstars <searchstars@163.com>
2025-10-25 16:46:09 +0800

    fmt

--- src/tools.rs
+++ src/tools.rs
@@ -15,7 +15,9 @@ pub fn hex_stream_to_bytes(hex: &str) -> Result<Vec<u8>, String> {
 
     (0..hex.len())
         .step_by(2)
-        .map(|i| u8::from_str_radix(&hex[i..i + 2], 16).map_err(|e| e.to_string()))
+        .map(|i| {
+            u8::from_str_radix(&hex[i..i + 2], 16).map_err(|e| format!("{:?}", e))
+        })
         .collect()
 }
 
